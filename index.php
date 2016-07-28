d639076ea85e26b3773bb4c06bf8edd24772c79b
Giles Sandoval <gsandoval2277@gmail.com>
2016-07-28 13:18:13 -0600

    learned the padding in dev tools and applied to php storm

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 			<h1>Persona Case #1</h1>
 
 			<!-- Persona section -->
-			<h2>Persona | John Smith | 25 years old</h2>
+			<h2>Persona | Joan Smith | 25 years old</h2>
 			<p id="first-paragraph">Profession: Professional Artist. Paintings mainly acrylic and water based</p>
 			<p id="first-paragraph">Technology: Macbook Pro, iPhone 6, and iPad uses to draw her skeletons of paintings. </p>
 			<p id="first-paragraph">Attitudes and behavior: Enjoys her time in the mornings to draw her paintings. She feels it calms her. She enjoys using her phone or iPad to give her ideas for her painting in the mornings. </p>
@@ -28,10 +28,10 @@
 
 			<!-- Use case section -->
 			<h2>Use Case</h2>
-			<p class="second-paragraph">test</p>
+			<p class="second-paragraph">content here</p>
 
 			<!-- Interaction Flow -->
-			<h2 class="third-paragraph">Interaction Flow</h2>
+			<h2>Interaction Flow</h2>
 			<ul style="
     background-color: turquoise;
     border: 5px solid darkgrey;
@@ -40,47 +40,12 @@
     padding: 20px;
     margin: 20px;
 ">
-				<li>“I want to enter a verse in the search field.”</li>
-				<li>“I want to see the verse in the results field and read it.”</li>
-				<li>“I would like the option to read multiple versions of the English bible, for reference.”</li>
-				<li>“I want to see a list of each word within that verse.”</li>
-				<li>“I want to see the verse in English and in the original Greek/Hebrew parallel to it.”</li>
-				<li>“I want to click the link of the Greek/Hebrew word to lead me to the original Greek/Hebrew definition.”</li>
-				<li>“I want a button to let me know how it is pronounced.”</li>
-				<li>“I want a button that gives me information about the tense, voice, and mood of the word.”</li>
-				<li>“I’d also like to know how many times the word is used in the entire bible.”</li>
-				<li>“I would like a help button to let help me in case I have questions on what certain buttons or links do.”</li>
-				<li>“I would like a button to arrange how the sentence is said in English and how the sentence is arranged in Greek form.”</li>
-				<li>“I would like a reference to what version of the bible I’m reading in English and what original scroll was used to translate that version in the original scroll of Greek/Hebrew”</li>
-				<li>“I would like to be able to click a word and see how many times that word was used in that version in the bible.” </li>
-				<li>“I want to click on one of the words that gives me all the multiple meanings of that word in the Greek.” </li>
-				<li>“I want to be able to read multiple English versions parallel to each other when I’m studying.”</li>
+				<li>“I want to enter a website that I can upload my portfolio”</li>
+				<li>“I want a place I can leave links to my fb, linkedin, website, and business”</li>
+				<li>“I would like to be able to be contacted by my preferred method of contact. ”</li>
+				<li>“I would like to be able to reach out to small businesses or people interested in displaying my artwork”</li>
+
 			</ul>
-
-			<!-- User flow content -->
-			<h2>User Flow</h2>
-			<ul>
-				<li style="list-style-type: none;">0)	Users/non-users don't need an account to look up verses & words.</li>
-				<li style="list-style-type: none;">1)	User types in “money” into the verse search or lexicon search field.</li>
-				<li style="list-style-type: none;">2)	User has a list of verses with the word money to choose from.</li>
-				<li style="list-style-type: none;">3)	User clicks on the relevant verse.</li>
-				<li style="list-style-type: none;">3)	User (users with accounts only) can favorite a verse, to refer back to later.</li>
-				<li style="list-style-type: none;">4)	User expands to see each word’s individual Greek/Hebrew word and meaning.</li>
-				<li style="list-style-type: none;">5)	User selects the word money with the Greek word right next to it.</li>
-				<li style="list-style-type: none;">6)	User sees a list of definitions or multiple meanings of the word “money” in the Greek/Hebrew language.</li>
-			</ul>
-
-			<!-- Conceptual model image here - needs approval from instructor -->
-			<h2>Conceptual Model</h2>
-			<ul>
-				<li style="list-style-type: none">Profile can search for many verses.</li>
-				<li style="list-style-type: none">Profile can do many word searches in the lexicon/dictionary.</li>
-				<li style="list-style-type: none">Profile can favorite a verse.</li>
-				<li style="list-style-type: none">Profile can open the list of words (lexicon list) within that verse.</li>
-				<li style="list-style-type: none">Profile can click on a certain word to see the greek/hebrew meaning of that specific word.</li>
-			</ul>
-
-
 
 		</main>
 	</body>
