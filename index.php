ad9dbed74eda4b66cf31982e5ea88f7d6c8c23ef
Giles Sandoval <gsandoval2277@gmail.com>
2016-07-28 10:09:59 -0600

    edit content and did style sheets.

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html lang="utf-8">
 	<head>
+		<!-- css-->
+		<!-- this means it will read this code before it loads content. load css 1st then content 2nd-->
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+
+		<!-- adding custom JavaScript-->
+
 		<title>Persona Cases</title>
 	</head>
 	<body>
@@ -10,9 +16,9 @@
 
 			<!-- Persona section -->
 			<h2>Persona | John Smith | 25 years old</h2>
-			<p>Profession: </p>
-			<p>Technology: Windows 10 user on his laptop, Android phone, Samsung Galaxy 5s with up to date version on his phone, Uses his Samsung smart 60” TV to link his phone or his laptop to help him have multiple screens or watch videos.</p>
-			<p>Attitudes and behavior: He works in a small church with many events throughout the week that he is encouraged to attend. He spends anywhere from 8-12 hours a week preparing a message for the youth ministry. He is a mentor, counselor, and a leader for his youth that approach him for fellowship or to help them with biblical solutions to their problems. He gets easily frustrated with the lag of a site, if he’s browsing. Will wait for it to load, but dislikes it extremely when he has to wait. He prefers an online lexicon/dictionary over an actual huge dictionary he has to carry around to help him study. He’d rather have it on his phone or log on to a reliable website. If a site lets him down. His alternative will be to go to his concordance dictionary that is through his kindle app on his phone. He is a huge social media user, to stay connected to the friends and events in the church. He has accounts he frequents on Twitter, Instagram, Snapchat and facebook, so he is accessible to the ministry via social media. He is also on Linkedin, mainly to connect with other pastors in the church and other churches internationally. He also takes part in 2 mission trips per year, for about a week at a time. This enables him to access his tools via internet/phone/laptop.</p>
+			<p>Profession: Professional Artist. Paintings mainly acrylic and water based</p>
+			<p>Technology: Macbook Pro, iPhone 6, and iPad uses to draw her skeletons of paintings. </p>
+			<p>Attitudes and behavior: </p>
 			<p>Frustrations and needs: He is human. He has a point where he knows if he’s losing it, He has to unplug from the world and pray and read the word, and talk with God. He is in ministry, so he gets paid a minimum salary. He has a goal to write a book to hopefully supplement money for the church and his future as a youth minister and future pastor in the church. He uses his head pastor as a mentor to help him through certain situations and his pastor uses a concordance/lexicon for his bible studies. With all these responsibilities and his natural spirit to want to help others, it’s easy to feel he’s pulled in different directions. He needs his quiet time with God in the early mornings, in the evenings, and the occasional emergency times during the day.</p>
 			<p>Goals: His main goal is that when he speaks the youth understands where he’s coming from when explaining subjects, concepts, and definitions about the bible. His goal is to keep the message as simple as possible. He wants the youth ministry to apply it to their lives in a practical manner through painting a picture. He would like to keep his life as simple as he could so that when a student/friend/or Christ follower asks what he does or how he does it…He has specifics to give them. But they can tailor to what he does.</p>
 
