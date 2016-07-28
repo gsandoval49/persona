e6a2b011c23f3a0065f6426825ef80e7ad9c21ed
Giles Sandoval <gsandoval2277@gmail.com>
2016-07-28 09:45:54 -0600

    html language  content written for persona cases. Needs editing.

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="utf-8">
+	<head>
+		<title>Persona Cases</title>
+	</head>
+	<body>
+		<header>Persona Cases</header>
+		<main>
+			<h1>Persona Case #1</h1>
+
+			<!-- Persona section -->
+			<h2>Persona | Giles Sandoval | 30 years old</h2>
+			<p>Profession: Youth minister. Leads a group of youths between the ages of 18-29 years old. Performs weekly messages to youth during weekdays. Does his bible study in the mornings and once in the evening. Requires a lexicon for the Greek and Hebrew definitions of the bible. Compared to the English language, a single word in the Greek or Hebrew could have multiple meanings. He needs a lexicon to help translate a word from his English version of the bible. He reads from the New King James Version of the bible, which is a literal (“word-to-word”) translation. And for the more modern version he uses the Message version or the New Living Translation version as a “thought-to-thought” translation. He needs a Hebrew/Greek lexicon to help him dive deeper into the word.</p>
+			<p>Technology: Windows 10 user on his laptop, Android phone, Samsung Galaxy 5s with up to date version on his phone, Uses his Samsung smart 60” TV to link his phone or his laptop to help him have multiple screens or watch videos.</p>
+			<p>Attitudes and behavior: He works in a small church with many events throughout the week that he is encouraged to attend. He spends anywhere from 8-12 hours a week preparing a message for the youth ministry. He is a mentor, counselor, and a leader for his youth that approach him for fellowship or to help them with biblical solutions to their problems. He gets easily frustrated with the lag of a site, if he’s browsing. Will wait for it to load, but dislikes it extremely when he has to wait. He prefers an online lexicon/dictionary over an actual huge dictionary he has to carry around to help him study. He’d rather have it on his phone or log on to a reliable website. If a site lets him down. His alternative will be to go to his concordance dictionary that is through his kindle app on his phone. He is a huge social media user, to stay connected to the friends and events in the church. He has accounts he frequents on Twitter, Instagram, Snapchat and facebook, so he is accessible to the ministry via social media. He is also on Linkedin, mainly to connect with other pastors in the church and other churches internationally. He also takes part in 2 mission trips per year, for about a week at a time. This enables him to access his tools via internet/phone/laptop.</p>
+			<p>Frustrations and needs: He is human. He has a point where he knows if he’s losing it, He has to unplug from the world and pray and read the word, and talk with God. He is in ministry, so he gets paid a minimum salary. He has a goal to write a book to hopefully supplement money for the church and his future as a youth minister and future pastor in the church. He uses his head pastor as a mentor to help him through certain situations and his pastor uses a concordance/lexicon for his bible studies. With all these responsibilities and his natural spirit to want to help others, it’s easy to feel he’s pulled in different directions. He needs his quiet time with God in the early mornings, in the evenings, and the occasional emergency times during the day.</p>
+			<p>Goals: His main goal is that when he speaks the youth understands where he’s coming from when explaining subjects, concepts, and definitions about the bible. His goal is to keep the message as simple as possible. He wants the youth ministry to apply it to their lives in a practical manner through painting a picture. He would like to keep his life as simple as he could so that when a student/friend/or Christ follower asks what he does or how he does it…He has specifics to give them. But they can tailor to what he does.</p>
+
+			<!-- Use case section -->
+			<h2>Use Case</h2>
+			<p>His lead pastor gave him a task to speak on finances for the weekly message coming up. The lead pastor feels that it’s a subject that should be addressed to the youth ministry. He wants him to create a message tailored around the original Greek/Hebrew definitions of 2 specific verses. Over 50% of Jesus ministry were financial principles. The pastor gave him 2 verses to dive into and create a message surrounding those 2 verses. He’s welcome to refer to other verses with that SAME word used in the Greek/Hebrew, to help support the 2 main verses. He will use the lexicon/dictionary to define specific words in the verse. He will use those definitions to paint a picture of how that specific word was used in that context, in that time period, in that culture, and how that word applies to our youth today.</p>
+
+			<!-- Interaction Flow -->
+			<h2>Interaction Flow</h2>
+			<ul>
+				<li>“I want to enter a verse in the search field.”</li>
+				<li>“I want to see the verse in the results field and read it.”</li>
+				<li>“I would like the option to read multiple versions of the English bible, for reference.”</li>
+				<li>“I want to see a list of each word within that verse.”</li>
+				<li>“I want to see the verse in English and in the original Greek/Hebrew parallel to it.”</li>
+				<li>“I want to click the link of the Greek/Hebrew word to lead me to the original Greek/Hebrew definition.”</li>
+				<li>“I want a button to let me know how it is pronounced.”</li>
+				<li>“I want a button that gives me information about the tense, voice, and mood of the word.”</li>
+				<li>“I’d also like to know how many times the word is used in the entire bible.”</li>
+				<li>“I would like a help button to let help me in case I have questions on what certain buttons or links do.”</li>
+				<li>“I would like a button to arrange how the sentence is said in English and how the sentence is arranged in Greek form.”</li>
+				<li>“I would like a reference to what version of the bible I’m reading in English and what original scroll was used to translate that version in the original scroll of Greek/Hebrew”</li>
+				<li>“I would like to be able to click a word and see how many times that word was used in that version in the bible.” </li>
+				<li>“I want to click on one of the words that gives me all the multiple meanings of that word in the Greek.” </li>
+				<li>“I want to be able to read multiple English versions parallel to each other when I’m studying.”</li>
+			</ul>
+
+			<!-- User flow content -->
+			<h2>User Flow</h2>
+			<ul>
+				<li style="list-style-type: none;">0)	Users/non-users don't need an account to look up verses & words.</li>
+				<li style="list-style-type: none;">1)	User types in “money” into the verse search or lexicon search field.</li>
+				<li style="list-style-type: none;">2)	User has a list of verses with the word money to choose from.</li>
+				<li style="list-style-type: none;">3)	User clicks on the relevant verse.</li>
+				<li style="list-style-type: none;">3)	User (users with accounts only) can favorite a verse, to refer back to later.</li>
+				<li style="list-style-type: none;">4)	User expands to see each word’s individual Greek/Hebrew word and meaning.</li>
+				<li style="list-style-type: none;">5)	User selects the word money with the Greek word right next to it.</li>
+				<li style="list-style-type: none;">6)	User sees a list of definitions or multiple meanings of the word “money” in the Greek/Hebrew language.</li>
+			</ul>
+
+			<!-- Conceptual model image here - needs approval from instructor -->
+			<h2>Conceptual Model</h2>
+			<ul>
+				<li style="list-style-type: none">Profile can search for many verses.</li>
+				<li style="list-style-type: none">Profile can do many word searches in the lexicon/dictionary.</li>
+				<li style="list-style-type: none">Profile can favorite a verse.</li>
+				<li style="list-style-type: none">Profile can open the list of words (lexicon list) within that verse.</li>
+				<li style="list-style-type: none">Profile can click on a certain word to see the greek/hebrew meaning of that specific word.</li>
+			</ul>
+
+
+
+		</main>
+	</body>
+	<footer>
+
+	</footer>
+</html>
