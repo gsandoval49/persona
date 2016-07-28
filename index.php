a6440cdfc530fe329986ce45a851feba6b08ba03
Giles Sandoval <gsandoval2277@gmail.com>
2016-07-28 11:02:48 -0600

    introduced color with class and id tags and element selectors

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 
 			<!-- Persona section -->
 			<h2>Persona | John Smith | 25 years old</h2>
-			<p class="pretty-title red-text">Profession: Professional Artist. Paintings mainly acrylic and water based</p>
+			<p id="first-paragraph">Profession: Professional Artist. Paintings mainly acrylic and water based</p>
 			<p class="pretty-title red-text">Technology: Macbook Pro, iPhone 6, and iPad uses to draw her skeletons of paintings. </p>
 			<p class="pretty-title red-text">Attitudes and behavior: Enjoys her time in the mornings to draw her paintings. She feels it calms her. She enjoys using her phone or iPad to give her ideas for her painting in the mornings. </p>
 			<p class="pretty-title red-text">Frustrations and needs: She dislikes having to wait for her pages to load. She'll stay on something for 5 seconds and if a page doesn't load she'll be done with it. She needs a platform where she can showcase her most recent work. She enjoys doing events and hanging her work at the local coffee shop down the street. But if she can, she'll avoid having to drive. </p>
